6cf6e3588e2954b4510ec2bd025a887a9a42abff
naz1ne <fabio.sirigu.1995@gmail.com>
2022-12-16 10:17:37 +0100

    add correction markup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,24 +19,25 @@
                   <a class="nav-link text-light" href="{{route('about')}}">About</a>
 
             </nav>
-            <main>
-                  <div class="container">
-                        <div class="title text-center">
-                              <h1>Primi passi in laravel</h1>
-                              <h2>Il mio primo saluto:</h2>
-                              <h2 class="text-danger">{{ $title }}{{ $class }}</h2>
-                        </div>
+      </header>
+      <main>
+            <div class="container">
+                  <div class="title text-center">
+                        <h1>Primi passi in laravel</h1>
+                        <h2>Il mio primo saluto:</h2>
+                        <h2 class="text-danger">{{ $title }}{{ $class }}</h2>
                   </div>
-            </main>
-            <footer>
-                  <!-- place footer here -->
-            </footer>
-            <!-- Bootstrap JavaScript Libraries -->
-            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
-            </script>
+            </div>
+      </main>
+      <footer>
+            <!-- place footer here -->
+      </footer>
+      <!-- Bootstrap JavaScript Libraries -->
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+      </script>
 
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
-            </script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+      </script>
 </body>
 
 </html>
